91669afc5401198339182110476c6b8642f76991
Alexandre SOARES <72209227+AS-37@users.noreply.github.com>
2022-02-15 19:56:25 +0100

    majj

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ app.post('/', async (req, res) => {
   };
 });
 
+app.post('/cats', async (req, res) => {
+  var cats =  (await axios.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=3')).data
+  return {
+    cats.map(cat => cat.text)
+  };
+});
+
+app.post('/foxes', async (req, res) => {
+  var foxes =  (await axios.get('https://randomfox.ca/floof/')).data
+  return {
+    foxes.image
+  };
+});
+
+app.post('/holidays', async (req, res) => {
+ var holidays = (await axios.get('https://date.nager.at/api/v3/publicholidays/2022/' + countryCode)).data
+  return {
+    holidays
+  };
+});
+
+
+
+
 // Only used for dev server, do not remove
 app.head('/', () => ({ ping: 'pong' }));
 
